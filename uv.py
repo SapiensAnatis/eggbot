418b83e346653dd7673be560fc3fa6183c1c9944
Jay <jay0@me.com>
2020-08-01 01:10:10 +0100

    fixed: wrong variable reference in url downloader

--- uv.py
+++ uv.py
@@ -8,7 +8,7 @@ class ImageURLData(commands.Converter):
     async def convert(self, ctx, argument):
         if utils.is_url(argument):
             async with aiohttp.ClientSession() as session:
-                async with session.get(url) as response:
+                async with session.get(argument) as response:
                     if response.status == 200:
                         img_data = response.read()
                         return img_data
