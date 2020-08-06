481a7d2f48ff6e67843034439505e0b09e6dd0dd
Jay <jay0@me.com>
2020-08-06 03:59:50 +0100

    save images, copy models, and generate scripts

--- uv.py
+++ uv.py
@@ -2,8 +2,14 @@ import discord
 import typing
 import aiohttp
 import utils
+import os
+import shutil
 from discord.ext import commands
 
+
+"""
+Converters to grab texture data using discord.ext.commands
+"""
 class ImageURLData(commands.Converter):
     async def convert(self, ctx, argument):
         if utils.is_url(argument):
@@ -18,6 +24,7 @@ class ImageURLData(commands.Converter):
 class UserAvatarData(commands.UserConverter):
     async def convert(self, ctx, argument):
         user = await super().convert(ctx, argument)
+        utils.log(f"Grabbing texture data of user {user.name}...")
         asset = user.avatar_url_as(format="png")
         img_data = await asset.read()
         return img_data
@@ -35,12 +42,12 @@ class AttachmentData(commands.Converter):
             # Select only the first attachment with an image extension
             # The bot could probably be adapted to do multiple renders,
             # but I don't want one user clogging up the entire queue. 
-            #
+            
             # Maybe a maximum of 3 to 5? It's something to look into
 
             attachment = self.get_valid_attachment(ctx.message.attachments)
             if attachment is None:
-                raise discord.ext.commands.BadArgument() # no attachments with correct type
+                raise discord.ext.commands.BadArgument() # no attachments w/ correct type
 
             img_data = await attachment.read()
             return img_data
@@ -62,26 +69,80 @@ class AttachmentData(commands.Converter):
 class UVCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        # Reset job ID as zero
+        self.job_id = 0
+        # Get valid shapes list
+        self.blend_files_dir = os.path.join(utils.get_bot_dir(), "models")
+        blend_files = os.listdir(self.blend_files_dir)
+        self.shapes = [f[:-6] for f in blend_files]
+        
+        # Canned error messages that I didn't want to repeat
+        self.no_img_data_err = ("Could not get any image data from your message.\n" +
+                           "I can take image data from image URLs, user avatars " +
+                           "(@mention or id), emojis, and image attachments.")
+        
+        self.no_valid_shape_err = "You must give a valid shape. Options are:"
+        for s in self.shapes:
+            self.no_valid_shape_err += f"\n\t• {s}"
 
     @commands.command(name='map', aliases=['uv'])
     async def prepare_render(
-        self, ctx, 
+        self, 
+        ctx, 
         shape: str,
-        texture_arg: typing.Union[ImageURLData, UserAvatarData, EmojiImageData, AttachmentData]
-        ):
+        tex_data: typing.Union[
+            ImageURLData, UserAvatarData, EmojiImageData, AttachmentData
+        ]
+    ):
+        f"""
+        Render a shape with a custom texture.
+        Arguments:
+            - shape: any of {self.shapes}
+            - tex_data: can be a URL, user mention/ID, emoji or image attachment ({utils.image_extensions})
         """
-        Converters grab texture data, this saves it and queues up the job 
-        """
+        # Check that shape is valid
+        if not (shape in self.shapes):
+            await ctx.send(self.no_valid_shape_err)
+            return
+        
+        # Increment job ID
+        self.job_id += 1
+        
+        # Would love to do async file I/O just for overkill purposes but that's more trouble
+        # than it's worth, even for showoff points
+
+        # Save texture image file
+        with open(f"job{self.job_id}_texture.image", "wb") as f: 
+            # Blender doesn't really care what the extension is
+            f.write(tex_data)
+        
+        # Copy .blend file
+        blend_filepath_src = os.path.join(self.blend_files_dir, shape + ".blend")
+        blend_filepath_dst = os.path.join(utils.get_bot_dir(), f"job{self.job_id}_model.blend")
+        shutil.copy(blend_filepath_src, blend_filepath_dst)
+
+        # Generate script to apply texture
+        with open(f"job{self.job_id}_script.py", "w") as f:
+            f.write(
+                f"import bpy\nbpy.data.images[\"texture\"].filepath = \"//job{self.job_id}_texture.image\""
+            )
+        
+        # Render
         
 
 
-"""
-how to use arbitrary textures:
 
-1. copy base blend file and texture to its own folder
-2. run python script in blend file to grab filepath of texture in that folder and set the texture reference to it
-3. render
-"""
+    @prepare_render.error
+    async def prepare_render_handler(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            if error.param.name == "shape":
+                await ctx.send(self.no_valid_shape_err)
+            if error.param.name == "tex_data":
+                await ctx.send(self.no_img_data_err)
+                           
+        if isinstance(error, commands.BadUnionArgument):
+            await ctx.send(self.no_img_data_err)
+        
 
 def setup(bot):
     bot.add_cog(UVCog(bot))
